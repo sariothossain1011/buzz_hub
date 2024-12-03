cb6907c6a6ae4ab567e69ed979cc74c3d7a0875d
sariothossain1011 <sariothossain1011@gmail.com>
2024-12-04 01:01:26 +0600

    updated

--- src/axios/axiosInstance.ts
+++ src/axios/axiosInstance.ts
@@ -6,7 +6,7 @@ import Cookies from "js-cookie";
 const instance = axios.create();
 instance.defaults.timeout = 60000;
 
-instance.defaults.baseURL = "http://localhost:8080/api/v1/";
+instance.defaults.baseURL = "https://buzz-hub-backend.onrender.com/api/v1/";
 
 // Add a request interceptor
 instance.interceptors.request.use(
